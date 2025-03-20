c5ca90a62537193035f2a3c2985bb1eb3a6c75f7
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-20 08:06:48 -0700

    Update Docker run command in BUILD.sh to reflect new package name and port change

--- BUILD.sh
+++ BUILD.sh
@@ -1,3 +1,3 @@
 git pull;
 trunk build --release;
-sudo docker run -d -v /home/teresa/mtvleptos/dist:/usr/share/nginx/html:ro -p 9000:8080 nginx:bookworm;
+sudo docker run -d -v /home/teresa/mtv-tv-leptos/dist:/usr/share/nginx/html:ro -p 9090:80 nginx:bookworm;
