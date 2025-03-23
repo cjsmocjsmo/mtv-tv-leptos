957fa5d21ab6ae34e2b5f608bfeda4d8e41cf00d
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-23 09:14:15 -0700

    Add Dockerfiles for arm32 and arm64 architectures and update BUILD.sh paths

--- BUILD.sh
+++ BUILD.sh
@@ -8,11 +8,11 @@ arch=$(uname -m);
 echo "$arch";
 
 if [ "$arch" = "aarch64" ]; then
-    docker build -t mtvtvlep:arm64 -f Dockerfile64 .
+    docker build -t mtvtvlep:arm64 -f ./arm64/Dockerfile .
     docker run -d -p 9093:80 mtvtvlep:arm64
 fi
 
 if [ "$arch" = "armv7l" ]; then
-    docker build -t mtvtvlep:arm32 -f Dockerfile32 .
+    docker build -t mtvtvlep:arm32 -f ./arm32/Dockerfile .
     docker run -d -p 9093:80 mtvtvlep:arm32
 fi
