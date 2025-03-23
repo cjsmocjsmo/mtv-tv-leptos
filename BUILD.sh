e5db1d0270e53844e99c4977cf1dbdea25e0e129
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-23 10:01:40 -0700

    Add support for amd64 architecture in BUILD.sh and create corresponding Dockerfile

--- BUILD.sh
+++ BUILD.sh
@@ -2,10 +2,10 @@
 cd /home/whitepi/mtv-tv-leptos;
 
 git pull;
+
 trunk build --release;
 
 arch=$(uname -m);
-echo "$arch";
 
 if [ "$arch" = "aarch64" ]; then
     docker build -t mtvtvlep:arm64 -f ./arm64/Dockerfile .
@@ -15,4 +15,9 @@ fi
 if [ "$arch" = "armv7l" ]; then
     docker build -t mtvtvlep:arm32 -f ./arm32/Dockerfile .
     docker run -d -p 9093:80 mtvtvlep:arm32
+fi
+
+if [ "$arch" = "x86_64" ]; then
+    docker build -t mtvtvlep:amd64 -f ./amd64/Dockerfile .
+    docker run -d -p 9093:80 mtvtvlep:amd64
 fi
