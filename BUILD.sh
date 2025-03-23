0c503d5c9dabc72716c5928e491e0359834e32dd
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-23 00:23:25 -0700

    Refactor BUILD.sh to use double brackets for conditional checks and ensure proper execution for ARM architectures

--- BUILD.sh
+++ BUILD.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd /home/whitepi/mtv-tv-leptos;
 
 git pull;
@@ -5,12 +6,12 @@ trunk build --release;
 
 arch=$(uname -m);
 
-if [ "$arch" == "aarch64" ]; then
+if [[ "$arch" == "aarch64" ]]; then
     docker build -t mtvtvlep:arm64 -f Dockerfile64 .
     docker run -d -p 9093:80 mtvtvlep:arm64
 fi
 
-if [ "$arch" == "armv7l" ]; then
+if [[ "$arch" == "armv7l" ]]; then
     docker build -t mtvtvlep:arm32 -f Dockerfile32 .
     docker run -d -p 9093:80 mtvtvlep:arm32
 fi
