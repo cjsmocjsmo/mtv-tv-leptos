f93b79f221c319e3ca695ce67482cfaaeb007c7a
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-23 00:26:43 -0700

    Fix conditional checks in BUILD.sh to use single brackets for architecture detection

--- BUILD.sh
+++ BUILD.sh
@@ -7,12 +7,12 @@ trunk build --release;
 arch=$(uname -m);
 echo $arch;
 
-if [[ "$arch" == "aarch64" ]]; then
+if [ $arch == "aarch64" ]; then
     docker build -t mtvtvlep:arm64 -f Dockerfile64 .
     docker run -d -p 9093:80 mtvtvlep:arm64
 fi
 
-if [[ "$arch" == "armv7l" ]]; then
+if [ $arch == "armv7l" ]; then
     docker build -t mtvtvlep:arm32 -f Dockerfile32 .
     docker run -d -p 9093:80 mtvtvlep:arm32
 fi
